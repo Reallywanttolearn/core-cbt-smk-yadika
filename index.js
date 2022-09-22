04fd976a4f276c82b34b87a90bae57fc3a9f3be6
themas febrianto <themasfebrianto13@gmail.com>
2022-09-22 14:47:04 +0700

    add home page

---
+++ index.js
@@ -0,0 +1,15 @@
+var toglak = () => {
+    document.getElementById('akuntansi').style.display = "block"
+    document.getElementById('rpl').style.display = "none"
+    document.getElementById('hotel').style.display = "none"
+}
+var toglrpl = () => {
+    document.getElementById('akuntansi').style.display = "none"
+    document.getElementById('rpl').style.display = "block"
+    document.getElementById('hotel').style.display = "none"
+}
+var toglhtl = () => {
+    document.getElementById('akuntansi').style.display = "none"
+    document.getElementById('rpl').style.display = "none"
+    document.getElementById('hotel').style.display = "block"
+}
